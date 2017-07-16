a5afb164338e077ad273ed8ec02e9bd242e97d7d
Willi Krappen <wkrappen91@yahoo.de>
2017-07-16 03:50:04 +0200

    ?

--- js/model/Map.js
+++ js/model/Map.js
@@ -5,17 +5,31 @@ var collision = require('../../modules/collision/collision').collision;
 var Player_1 = require("./Player");
 var Map = (function () {
     function Map() {
-        this.bucketSize = 100;
+        this.bucketSize = 10;
         this.range = {
             x: 0,
             y: 0,
-            w: 5000,
-            h: 5000
+            w: 500,
+            h: 500
         };
         this.hash = new SpatialHash(this.range, this.bucketSize);
-        var m = new Player_1["default"]({ x: 1, y: 1 }, 10);
-        var m2 = new Player_1["default"]({ x: 4, y: 4 }, 10);
-        console.log(m);
+        var m = new Player_1["default"]({
+            x: 1,
+            y: 1
+        }, 10);
+        var m2 = new Player_1["default"]({
+            x: 50,
+            y: 50
+        }, 10);
+        this.hash.insert(m);
+        this.hash.insert(m2);
+        console.log("item count", this.hash.itemCount);
+        console.log("query", this.hash.query({
+            x: 0,
+            y: 0,
+            w: 1,
+            h: 1
+        }));
         console.log(collision(m, m2));
     }
     return Map;
--- ts/model/Map.ts
+++ ts/model/Map.ts
@@ -3,7 +3,7 @@ const collision = require('../../modules/collision/collision').collision;
 import Player from "./Player";
 export default class Map {
 	hash: any;
-	bucketSize:number = 100;
+	bucketSize: number = 10;
 	range: {
 		x: number,
 		y: number,
@@ -12,17 +12,31 @@ export default class Map {
 	} = {
 		x: 0,
 		y: 0,
-		w: 5000,
-		h: 5000
+		w: 500,
+		h: 500
 	};
 
 	constructor() {
 		this.hash = new SpatialHash(this.range, this.bucketSize);
 
-		var m = new Player({x:1,y:1}, 10);	
-		var m2 = new Player({x:4,y:4}, 10);	
-		console.log(m);
-
-		console.log(collision(m,m2));
+		var m = new Player({
+			x: 1,
+			y: 1
+		}, 10);
+		var m2 = new Player({
+			x: 50,
+			y: 50
+		}, 10);
+		this.hash.insert(m);
+		this.hash.insert(m2);
+		console.log("item count", this.hash.itemCount);
+		console.log("query", this.hash.query({
+			x: 0,
+			y: 0,
+			w: 1,
+			h: 1
+		}));
+		
+		console.log(collision(m, m2));
 	}
 }
